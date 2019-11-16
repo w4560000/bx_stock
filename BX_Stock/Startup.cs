420417371076c7b1ad95dc0ea6ea33138792b7e0
BingXiang <a0916364056@gmail.com>
2019-11-16 14:49:51 +0800

    test

--- BX_Stock/Startup.cs
+++ BX_Stock/Startup.cs
@@ -74,7 +74,7 @@ namespace BX_Stock
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Test}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
             // ³]©whangfire»öªíªO
